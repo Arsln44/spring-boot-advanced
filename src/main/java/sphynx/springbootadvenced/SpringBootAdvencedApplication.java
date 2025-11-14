ce4648dfb4bb0697a739c0f16d43f01391b55078
Burak Alp Arslan <arslnburak2001@gmail.com>
2025-11-14 04:16:32 +0300

    Initial commit: Spring Boot project setup

---
+++ src/main/java/sphynx/springbootadvenced/SpringBootAdvencedApplication.java
@@ -0,0 +1,13 @@
+package sphynx.springbootadvenced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdvencedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdvencedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sphynx/springbootadvenced/SpringBootAdvencedApplicationTests.java
@@ -0,0 +1,13 @@
+package sphynx.springbootadvenced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdvencedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
